dc5e4795c41b0d7bb9930e8e13e3108ebf090b19
Natalia Iukhimenko <natalia.yukhimenko@gmail.com>
2022-05-24 17:19:43 +0300

    Refactoring of VehicleStatusApiService

--- src/main/java/ru/iukhimenko/vehiclestatusapi/services/VehicleStatusApiService.java
+++ src/main/java/ru/iukhimenko/vehiclestatusapi/services/VehicleStatusApiService.java
@@ -1,12 +1,18 @@
 package ru.iukhimenko.vehiclestatusapi.services;
 
 import ru.iukhimenko.vehiclestatusapi.assertions.AssertableResponse;
+import ru.iukhimenko.vehiclestatusapi.responses.RoofTopStatusResponse;
 
 public class VehicleStatusApiService extends ApiService {
-    public AssertableResponse getRoofTopStatus(String vehicleId) {
+    public AssertableResponse getRoofTopStatusResponse(String vehicleId) {
         return new AssertableResponse(getAuthorizedSpec()
                 .pathParam("vehicleId", vehicleId)
                 .get("/vehicles/{vehicleId}/resources/rooftopstatus")
         );
+    }
+
+    public RoofTopStatusResponse getRoofTopStatus(String vehicleId) {
+        return getRoofTopStatusResponse(vehicleId)
+                .asObject(RoofTopStatusResponse.class);
     }
 }
