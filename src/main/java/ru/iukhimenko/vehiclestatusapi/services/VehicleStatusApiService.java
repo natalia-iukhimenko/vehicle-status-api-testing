3a7e3b55a49b4e85d9d394caaf34bc79abeeb345
Natalia Iukhimenko <natalia.yukhimenko@gmail.com>
2022-05-24 23:08:30 +0300

    Fixed a problem with import

--- src/main/java/ru/iukhimenko/vehiclestatusapi/services/VehicleStatusApiService.java
+++ src/main/java/ru/iukhimenko/vehiclestatusapi/services/VehicleStatusApiService.java
@@ -2,9 +2,8 @@ package ru.iukhimenko.vehiclestatusapi.services;
 
 import org.apache.http.HttpStatus;
 import ru.iukhimenko.vehiclestatusapi.assertions.AssertableResponse;
+import ru.iukhimenko.vehiclestatusapi.conditions.StandardConditions;
 import ru.iukhimenko.vehiclestatusapi.responses.RoofTopStatusResponse;
-
-import static ru.iukhimenko.vehiclestatusapi.conditions.StandardConditions.statusCode;
 
 public class VehicleStatusApiService extends ApiService {
     public AssertableResponse getRoofTopStatusResponse(String vehicleId) {
@@ -16,7 +15,7 @@ public class VehicleStatusApiService extends ApiService {
 
     public RoofTopStatusResponse getRoofTopStatus(String vehicleId) {
         return getRoofTopStatusResponse(vehicleId)
-                .shouldHave(statusCode(HttpStatus.SC_OK))
+                .shouldHave(StandardConditions.statusCode(HttpStatus.SC_OK))
                 .asObject(RoofTopStatusResponse.class);
     }
 }
