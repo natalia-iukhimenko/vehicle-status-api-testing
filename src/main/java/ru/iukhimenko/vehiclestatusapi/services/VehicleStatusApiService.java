f75d08781df3a5e5d6d1ad326ea1495bfbc96df6
Natalia Iukhimenko <natalia.yukhimenko@gmail.com>
2022-05-24 17:47:31 +0300

    Added status code check

--- src/main/java/ru/iukhimenko/vehiclestatusapi/services/VehicleStatusApiService.java
+++ src/main/java/ru/iukhimenko/vehiclestatusapi/services/VehicleStatusApiService.java
@@ -1,7 +1,10 @@
 package ru.iukhimenko.vehiclestatusapi.services;
 
+import org.apache.http.HttpStatus;
 import ru.iukhimenko.vehiclestatusapi.assertions.AssertableResponse;
 import ru.iukhimenko.vehiclestatusapi.responses.RoofTopStatusResponse;
+
+import static ru.iukhimenko.vehiclestatusapi.conditions.StandardConditions.statusCode;
 
 public class VehicleStatusApiService extends ApiService {
     public AssertableResponse getRoofTopStatusResponse(String vehicleId) {
@@ -13,6 +16,7 @@ public class VehicleStatusApiService extends ApiService {
 
     public RoofTopStatusResponse getRoofTopStatus(String vehicleId) {
         return getRoofTopStatusResponse(vehicleId)
+                .shouldHave(statusCode(HttpStatus.SC_OK))
                 .asObject(RoofTopStatusResponse.class);
     }
 }
